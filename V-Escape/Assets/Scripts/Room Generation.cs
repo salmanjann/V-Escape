89c670b50e6ef98abecceca476b74360420376b6
Abdullah Shahid Butt <abdullahshahid210@gmail.com>
2024-10-10 12:48:14 +0500

    Room Layouts
    Room Layouts Generated Successfully

--- V-Escape/Assets/Scripts/Room Generation.cs
+++ V-Escape/Assets/Scripts/Room Generation.cs
@@ -20,12 +20,15 @@ public class RoomGeneration : MonoBehaviour
     void Awake()
     {
         // Make an empty canvas on which the blueprint will be drawn using that 2d blueprint objects in 3d will be placed 
-        int canvas_size = 80;
+        int canvas_size = 1000;
         // set the max size the hall can have (bad approach but gotta roll with this one)
         int hall_size = 20;
+        // set the max size the room can have (bad approach but gotta roll with this one)
+        int room_size = 9;
 
         // sets that hold integer ids describing what section on canvas is their region
         HashSet<int> Hall_ground_set = new HashSet<int>();
+        HashSet<int> Room_ground_set = new HashSet<int>();
         HashSet<int> Wall_set = new HashSet<int>();
 
         // This is to make unique blocks on the canvas as it increments once a block is drawin using this id
@@ -47,7 +50,7 @@ public class RoomGeneration : MonoBehaviour
                 array[i,j] = 0; 
 
         // assign starting position on canvas to the Hall
-        Vector2 start_hall = new Vector2(random.Next(canvas_size-hall_size-1),random.Next(canvas_size-hall_size-1));
+        Vector2 start_hall = new Vector2(random.Next(canvas_size/4,canvas_size/4*3),random.Next(canvas_size/4,canvas_size/4*3));
         // assign the hall its size and make sure its odd so that the hall will always havea center point.
         Vector2 size = new Vector2(random.Next(hall_size/8,hall_size/2)*2 + 1,random.Next(hall_size/8,hall_size/2)*2 + 1);
 
@@ -57,6 +60,73 @@ public class RoomGeneration : MonoBehaviour
                 array[i,j] = Items_counter; 
         Hall_ground_set.Add(Items_counter);
         Items_counter++;
+
+        // Check how many rooms can be placed along the hall in this way
+        Vector2 roomsfittable = new Vector2(size.x/room_size,size.y/room_size);
+
+        // Draw rooms on x wise sides if space is available on canvas and add a certain probablity that the room will not be made at all
+        for(int i = 0; i < roomsfittable.x; i++)
+        {
+            // Needs to be done for both sides of the hallway
+            for(int j = -1; j <= 1; j+=2)
+            {
+                // // random chance that room will not be made at all
+                // if(random.Next(100)<30)
+                // {
+                //     continue;
+                // }
+                Vector2 current_room_size = new Vector2(random.Next(room_size/4,room_size/2)*2 + 1,random.Next(room_size/4,room_size/2)*2 + 1);
+                int addfactor = 0;
+                if(j == 1)
+                {
+                    addfactor = 1;
+                }
+                int subtractfactor = 0;
+                if(j == -1)
+                {
+                    subtractfactor = 1;
+                }
+                Vector2 min = new Vector2(start_hall.x + room_size * i,start_hall.y + j + ((size.y - 1) * addfactor) - (subtractfactor * current_room_size.y-1));
+                Vector2 max = new Vector2(start_hall.x + room_size * i + current_room_size.x,start_hall.y + j + current_room_size.y + ((size.y - 1) * addfactor) - (subtractfactor * current_room_size.y-1));
+
+                
+                // see if the Room is going out of bounds. in that case dont draw the room at all
+                if(min.x < 0 && min.y < 0 && min.x >= canvas_size && min.y >= canvas_size)
+                {
+                    continue;
+                }
+                else if(max.x < 0 && max.y < 0 && max.x >= canvas_size && max.y >= canvas_size)
+                {
+                    continue;
+                }
+                // draw the room on canvas if all has passed
+                for(int a = (int)min.x; a < max.x; a++)
+                {
+                    for(int b = (int)min.y; b != max.y;)
+                    {
+                        if(a<0 || a>=canvas_size)
+                        {
+                            Debug.Log("a = "+a.ToString());
+                            Debug.Log(min.x + "" + max.x);
+                        }
+                        if(b<0 || b>=canvas_size)
+                        {
+                            Debug.Log("b = "+b.ToString());
+                            Debug.Log(min.y + "" + max.y);
+                        }
+                        array[a,b] = Items_counter;
+                        if(b<max.y)
+                            b++;
+                        if(b>max.y)
+                            b--;
+                    }
+                }
+                Room_ground_set.Add(Items_counter);
+                Items_counter++;
+            }
+        }
+
+        // do the same as above but on y side
 
         // draw walls for that block covering the borders horizontally and vertically
         for(int i = (int)start_hall.x; i < start_hall.x + size.x; i++)
@@ -77,30 +147,55 @@ public class RoomGeneration : MonoBehaviour
 
         // Empty parent nodes for the blocks on map so that they can be easily distinguished using inspector
         GameObject Hall_Floor_Parent = new GameObject("Hall Floor");
+        // GameObject Room_Floor_Parent = new GameObject("Room Floor");
+        List<GameObject> Room_Floor_Parents = new List<GameObject>();
+
+        foreach(int room in Room_ground_set)
+        {
+            GameObject temp = new GameObject("Room Floor "+room.ToSafeString());
+            Room_Floor_Parents.Add(temp);
+        }
         GameObject Wall_Parent = new GameObject("Wall");
 
         // Finally scan the whole blueprint and spawn objects accordingly
         for(int i = 0; i < canvas_size; i++)
         {   
             for(int j = 0; j < canvas_size; j++)
+            {
+                // if the id is in the hall section spawn a hall floor
+                if(Hall_ground_set.Contains(array[i,j]))
                 {
-                    // if the id is in the hall section spawn a hall floor
-                    if(Hall_ground_set.Contains(array[i,j]))
-                    {
-                        var temp = Instantiate(Hall_ground);
-                        temp.transform.position = new Vector3(i,0,j);
-                        temp.transform.SetParent(Hall_Floor_Parent.transform);
-                        counter++;
-                    }
-                    // if the id is in the wall section spawn a wall
-                    else if(Wall_set.Contains(array[i,j]))
-                    {
-                        var temp = Instantiate(Wall);
-                        temp.transform.position = new Vector3(i,0,j);
-                        temp.transform.SetParent(Wall_Parent.transform);
-                        counter++;
-                    }
+                    var temp = Instantiate(Hall_ground);
+                    temp.transform.position = new Vector3(i,0,j);
+                    temp.transform.SetParent(Hall_Floor_Parent.transform);
+                    counter++;
                 }
+                // if the id is in the wall section spawn a wall
+                else if(Wall_set.Contains(array[i,j]))
+                {
+                    var temp = Instantiate(Wall);
+                    temp.transform.position = new Vector3(i,0,j);
+                    temp.transform.SetParent(Wall_Parent.transform);
+                    counter++;
+                }
+                // if the id is in the room section spawn a wall
+                else if(Room_ground_set.Contains(array[i,j]))
+                {
+                    var temp = Instantiate(Room_ground);
+                    temp.transform.position = new Vector3(i,0,j);
+                    GameObject roomobj = null;
+                    foreach (GameObject o in Room_Floor_Parents)
+                    {
+                        if(o.name == "Room Floor "+array[i,j].ToSafeString())
+                        {
+                            roomobj = o;
+                            break;
+                        }
+                    } 
+                    temp.transform.SetParent(roomobj.transform);
+                    counter++;
+                }
+            }
         }
         // show how many objects just spawned
         Debug.Log(counter.ToString());

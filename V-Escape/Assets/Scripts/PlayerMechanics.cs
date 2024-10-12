560dbad7dc8066c48f09ea98a50caa3cde7bc9d1
AbdullahShahid7402 <abdullahshahid210@gmail.com>
2024-10-12 05:43:55 +0500

    Initial Setting
    initial setup of the player playground and script made for the developer to work on the branch

---
+++ V-Escape/Assets/Scripts/PlayerMechanics.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMechanics : MonoBehaviour
+{
+    public GameObject Camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Walk()
+    {
+        // make sure the forward backward go in same direction player is facing and the side ways movement is handled accordingly
+    }
+
+    private void Turn_Around()
+    {
+        // make rotation upon the whole game object accordingly
+    }
+}

c2c581291d529987fbb6003e6a8d1c527246114c
Rizwan Saleem <rizstar125@gmail.com>
2024-12-10 11:02:59 +0500

    Main Menu Navigations Added

--- Assets/Scripts/Main_Menu.cs
+++ Assets/Scripts/Main_Menu.cs
@@ -1,14 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Main_Menu : MonoBehaviour
 {
-    
+    public GameObject mainMenu;
+    public GameObject controls;
+    public GameObject settings;
+
+    /*public Slider volumeSlider;
+
+    public AudioSource audioSource;
+    public AudioClip background;*/
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainMenu.SetActive(true);
+        settings.SetActive(false);
+        controls.SetActive(false);
+/*
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            PlayerPrefs.SetFloat("Volume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+        audioSource.clip = background;
+        audioSource.Play();*/
     }
 
     // Update is called once per frame
@@ -16,4 +40,49 @@ public class Main_Menu : MonoBehaviour
     {
         
     }
+
+    public void settingbtn()
+    {
+        settings.SetActive(true);
+        controls.SetActive(false);
+    }
+
+    public void controlsbtn()
+    {
+        controls.SetActive(true);
+        settings.SetActive(false);
+    }
+
+    public void backtbn()
+    {
+        settings.SetActive(false);
+        controls.SetActive(false);
+    }
+
+    public void playtbtnclicked()
+    {
+        //SceneManager.LoadScene(1);
+        Debug.Log("Play");
+    }
+    /*
+    public void changeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    public void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("Volume", volumeSlider.value);
+    }
+
+    public void playButton()
+    {
+        audioSource.Play();
+    }*/
 }

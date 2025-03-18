140a87c3c211138a9d7fe027041e0cf7efef4722
Rizwan Saleem <rizstar125@gmail.com>
2025-03-19 02:49:17 +0500

    Revert "Complete Main Menu Design with Sounds"
    This reverts commit a7b1729b9426d0b31715d060459f76b9fc40b4f2.

--- Assets/Scripts/Main_Menu.cs
+++ Assets/Scripts/Main_Menu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Main_Menu : MonoBehaviour
 {
@@ -10,12 +10,10 @@ public class Main_Menu : MonoBehaviour
     public GameObject controls;
     public GameObject settings;
 
-    public Slider volumeSlider;
+    /*public Slider volumeSlider;
 
-
-
-    public Text sound;
-    private bool isMuted;
+    public AudioSource audioSource;
+    public AudioClip background;*/
 
     // Start is called before the first frame update
     void Start()
@@ -23,28 +21,18 @@ public class Main_Menu : MonoBehaviour
         mainMenu.SetActive(true);
         settings.SetActive(false);
         controls.SetActive(false);
-
+/*
         if (PlayerPrefs.HasKey("Volume"))
         {
             PlayerPrefs.SetFloat("Volume", 1);
-            loadVoulme();
+            Load();
         }
         else
         {
-            loadVoulme();
+            Load();
         }
-
-        if (!PlayerPrefs.HasKey("Muted"))
-        {
-            PlayerPrefs.SetInt("Muted", 0);
-            loadSound();
-        }
-        else
-        {
-            loadSound();
-        }
-        updateSoundText();
-        AudioListener.pause = isMuted;
+        audioSource.clip = background;
+        audioSource.Play();*/
     }
 
     // Update is called once per frame
@@ -65,74 +53,36 @@ public class Main_Menu : MonoBehaviour
         settings.SetActive(false);
     }
 
-    public void backbtn()
+    public void backtbn()
     {
         settings.SetActive(false);
         controls.SetActive(false);
     }
 
-    public void playbtnclicked()
+    public void playtbtnclicked()
     {
         //SceneManager.LoadScene(1);
         Debug.Log("Play");
     }
-
-    public void exitbtnclicked()
-    {
-        Application.Quit();
-    }
-    
+    /*
     public void changeVolume()
     {
         AudioListener.volume = volumeSlider.value;
-        saveVolume();
+        Save();
     }
 
-    public void loadVoulme()
+    public void Load()
     {
         volumeSlider.value = PlayerPrefs.GetFloat("Volume");
     }
 
-    public void saveVolume()
+    public void Save()
     {
         PlayerPrefs.SetFloat("Volume", volumeSlider.value);
     }
 
-    public void soundBtn()
+    public void playButton()
     {
-        if (isMuted == false)
-        {
-            isMuted = true;
-            AudioListener.pause = true;
-        }
-        else
-        {
-            isMuted = false;
-            AudioListener.pause = false;
-        }
-        saveSound();
-        updateSoundText();
-    }
-
-    public void loadSound()
-    {
-        isMuted = PlayerPrefs.GetInt("Muted") == 1;
-    }
-
-    public void saveSound()
-    {
-        PlayerPrefs.SetInt("Muted", isMuted ? 1 : 0);
-    }
-
-    public void updateSoundText()
-    {
-        if (isMuted == false)
-        {
-            sound.text = "ON";
-        }
-        else
-        {
-            sound.text = "OFF";
-        }
-    }
+        audioSource.Play();
+    }*/
 }

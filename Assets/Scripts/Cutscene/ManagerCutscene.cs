620d1a0cb29082cfdc45308779e928457a74efb4
AbdullahShahid7402 <abdullahshahid210@gmail.com>
2025-03-19 14:25:12 +0500

    compiler errors resolved. Fix: commented unnecessary imports

--- Assets/Scripts/Cutscene/ManagerCutscene.cs
+++ Assets/Scripts/Cutscene/ManagerCutscene.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.SearchService;
+// using UnityEditor.SearchService;
 using UnityEngine;
 
 public class ManagerCutscene : MonoBehaviour

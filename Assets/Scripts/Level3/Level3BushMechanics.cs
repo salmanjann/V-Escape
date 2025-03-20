6e7ff5d4cc965ac4e7a2979802fbf3e72c40fcb1
AbdullahShahid7402 <abdullahshahid210@gmail.com>
2025-03-20 07:49:50 +0500

    LOD on bushses changed

--- Assets/Scripts/Level3/Level3BushMechanics.cs
+++ Assets/Scripts/Level3/Level3BushMechanics.cs
@@ -15,13 +15,15 @@ public class Level3BushMechanics : MonoBehaviour
     void Update()
     {
         Vector3 pos = this.transform.position;
-        this.transform.position = new Vector3(pos.x,pos.y - 1f*Time.deltaTime,pos.z);
+        this.transform.position = new Vector3(pos.x, pos.y - 1f * Time.deltaTime,pos.z);
     }
     void OnTriggerEnter(Collider collision)
     {
       Destroy(this.GetComponent<BoxCollider>());  
       this.AddComponent<SphereCollider>();
-      this.AddComponent<Rigidbody>().isKinematic = true;
+      var rb = this.AddComponent<Rigidbody>();
+      if(rb!=null)
+        rb.isKinematic = true;
       Destroy(this.GetComponent<Level3BushMechanics>());  
     }
 }

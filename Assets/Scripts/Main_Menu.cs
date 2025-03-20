cb94649dff20f2a27b30d76954d3bf992b7167f0
Rizwan Saleem <rizstar125@gmail.com>
2025-03-20 07:40:13 +0500

    Main Menu Finalized with Sounds and Effects

--- Assets/Scripts/Main_Menu.cs
+++ Assets/Scripts/Main_Menu.cs
@@ -16,6 +16,13 @@ public class Main_Menu : MonoBehaviour
     public AudioSource audioSource;
     public Slider volumeSlider;
 
+    public Text musicON;
+    public Text musicOFF;
+    public bool isMuted;
+
+    public Text AudioBtn;
+    public Text ControlBtn;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,12 +35,24 @@ public class Main_Menu : MonoBehaviour
         if (PlayerPrefs.HasKey("Volume"))
         {
             PlayerPrefs.SetFloat("Volume", 1);
-            Load();
+            LoadVolume();
         }
         else
         {
-            Load();
+            LoadVolume();
         }
+
+        if (!PlayerPrefs.HasKey("Muted"))
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+            LoadMusic();
+        }
+        else
+        {
+            LoadMusic();
+        }
+        updateMusicIcon();
+        AudioListener.pause = isMuted;
     }
 
     // Update is called once per frame
@@ -77,28 +96,71 @@ public class Main_Menu : MonoBehaviour
     {
         controlPanel.SetActive(true);
         audioPanel.SetActive(false);
+        ControlBtn.color = Color.red;
+        AudioBtn.color = Color.white;
     }
 
     public void Audios()
     {
         audioPanel.SetActive(true);
         controlPanel.SetActive(false);
+        AudioBtn.color = Color.red;
+        ControlBtn.color = Color.white;
     }
 
     public void changeVolume()
     {
         AudioListener.volume = volumeSlider.value;
-        Save();
+        SaveVolume();
     }
 
-    public void Load()
+    public void LoadVolume()
     {
         volumeSlider.value = PlayerPrefs.GetFloat("Volume");
     }
 
-    public void Save()
+    public void SaveVolume()
     {
         PlayerPrefs.SetFloat("Volume", volumeSlider.value);
     }
 
+    public void musicBtn()
+    {
+        if (isMuted == false)
+        {
+            isMuted = true;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            isMuted = false;
+            AudioListener.pause = false;
+        }
+        SaveMusic();
+        updateMusicIcon();
+    }
+
+     public void updateMusicIcon()
+    {
+        if(isMuted == false)
+        {
+            musicON.enabled = true;
+            musicOFF.enabled = false;
+        }
+        else
+        {
+            musicON.enabled = false;
+            musicOFF.enabled = true;
+        }
+    }
+
+    public void LoadMusic()
+    {
+        isMuted = PlayerPrefs.GetInt("Muted") == 1;
+    }
+
+    public void SaveMusic()
+    {
+        PlayerPrefs.SetInt("Muted", isMuted ? 1 : 0);
+    }
 }
